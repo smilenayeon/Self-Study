bc2405b42424c716feb51fa07818e60a986f522b
Na Yeon <smilenayeon@naver.com>
2023-10-28 13:16:22 +0900

    updated Money componenet:users can input the amount to add or subtract.

--- src/componenets/Money.js
+++ src/componenets/Money.js
@@ -1,14 +1,24 @@
-import React, {useReducer} from 'react'
+import React, {useReducer, useState} from 'react'
 
 function Money() {
+
+const [amount, setAmount] =useState(0);
+
+
+
+const handleAmount = (e)=>{
+    
+    setAmount(e.target.value);
+    
+};
 
 const [money, moneyDispatch] = useReducer(moneyReducer,500);
 
 function moneyReducer(currentMoney, action){
-    if (action==="BUY"){
-        return  currentMoney-100;
-    }else if (action==='PAID'){
-        return currentMoney +200;
+    if (action ==='BUY'){
+        return  currentMoney - Number(amount);
+    }else if (action ==='PAID'){
+        return currentMoney + Number(amount);
     }
 
 }
@@ -20,6 +30,8 @@ function moneyReducer(currentMoney, action){
 
     <div>
 <h3>money:{money}</h3>
+<input type="money" placeholder="the amount" onChange={handleAmount}/>
+
 <button onClick={handleBuy}>Buy</button>
 <button onClick={handlePaid}>Get Paid</button>
     </div>
