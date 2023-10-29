05425aff26611f70b2ca6d1180e564ab40aa0304
Na Yeon <smilenayeon@naver.com>
2023-10-29 20:44:27 +0900

    password validation is added.

---
+++ src/componenets/PasswordErrorMessage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PasswordErrorMessage() {
+  return (
+    <div>Password Error Message: must be more than 8 digits</div>
+  )
+}
+
+export default PasswordErrorMessage;
--- src/componenets/SignUp.js
+++ src/componenets/SignUp.js
@@ -1,38 +1,33 @@
-import React,{useState} from 'react'
+import React,{useState} from 'react';
+import PasswordErrorMessage from "./PasswordErrorMessage";
 
 function SignUp() {
 
  const[firstName, setFirstName] = useState("");
     const[lastName, setLastName] = useState("");
     const[email, setEmail] = useState("");
-    const[password, setPassword] = useState({value:"" , isTouched:false});
+    const[password, setPassword] = useState("");///////////////working on password validation
     const[role, setRole] = useState("role");
-  
+
     const firstNameChange = (e)=>{setFirstName(e.target.value)};
     const lastNameChange = (e)=>{setLastName(e.target.value)};
     const emailChange = (e)=>{setEmail(e.target.value)};
-    const passwordChange = (e)=>{
-                                setPassword(e.target.value);
-                                if (userPassword.length>=8) {
-                                    setPassword(userPassword)
-                                }else{
-                                    alert("Try another password. Password should be 8 or more.");
-                                    setPassword("");
-                                }};
+    const passwordChange = (e)=>{setPassword({...password, value:e.target.value})};////////////////working on password validation
     const roleChange = (e)=>{setRole(e.target.value)};
 
     const clearForm=()=>{
         setFirstName("");
         setLastName("");
         setEmail("");
-        setPassword("");
+        setPassword({value:"", isTouched:false});
         setRole("role");
     };
 
     const handleSubmit= (e)=>{
         e.preventDefault();
-        alert("Account Created");
+        alert("successfullly signed up!");
         clearForm();
+
 
     };
 
@@ -52,7 +47,9 @@ function SignUp() {
             <input key="email" name="email" type="email" value={email} onChange={emailChange} required/>
 
             <label htmlFor="password">Password<sup>*</sup></label>
-            <input key="password" name="password" type="password" value={password} onChange={passwordChange} required/>
+            <input key="password" name="password" type="password" value={password.value} onChange={passwordChange} onBlur={()=>{setPassword({...password, isTouched:true})}} required/>
+            {password.isTouched && password.value.length < 8 ?  (<PasswordErrorMessage />): null }
+          
 
             <label htmlFor="Role">Role<sup>*</sup></label>
             <select onChange={roleChange} required>
