5636df96e06c1be8f7ee6d950f7997035d8b0d40
Na Yeon <smilenayeon@naver.com>
2023-11-01 19:18:19 +0900

    updated Survery component. textarea to provide reason for the score is added. submit is disabled when score is <6 and no reason was provided.

--- src/componenets/Survey.js
+++ src/componenets/Survey.js
@@ -1,24 +1,31 @@
 import React,{useState} from 'react'
 
 function Survey() {
-  const [score, setScore]=useState("(?)");
+  const [score, setScore]=useState(10);
+  const [comment,setComment]=useState("");
 
   const handleSubmit = (e)=>{
     e.preventDefault();
-    setScore("(?)");
+    setScore(10);
        alert("Thank you for the feedback!");
   };
-  return (
+ const isDisabled= Number(score) < 6 && comment.length < 10;
+
+  const textAreaPlaceholder = isDisabled ? "provide the reason(mininum 10 letters) please" : "this is optional";
+
+return (
 
     <div>
       <form onSubmit={handleSubmit}>
         <label htmlFor="ChefScore">Chef.A : {score}/10</label>
         <input type="range" min="0" max="10" value={score} name="chefScore" onChange={(e)=>setScore(e.target.value)}/>
-        <input type="submit" value="submit" name="submit"/>
+        <input type="submit" value="submit" name="submit" disabled={isDisabled}/>
+        <p>Why?</p>
+        <textarea name="comment" placeholder={textAreaPlaceholder} value={comment} onChange={(e)=>{setComment(e.target.value);}}></textarea>
       </form>
       
     </div>
-  )
-}
+  );
+};
 
 export default Survey;
