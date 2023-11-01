0743fbdce0dd794c28e60f6ee54e351841a330da
Na Yeon <smilenayeon@naver.com>
2023-11-02 06:21:27 +0900

    tested a simple funtion AddFive()

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import React from "react";
-import {screen, render, fireEvent } from '@testing-library/react';
+//import React from "react";
+//import {screen, render, fireEvent } from '@testing-library/react';
 import AddFive from "./componenets/AddFive";
 
 test('add 5 to the number', ()=>{
 
-    expect(1+5).toBe(6)
+    expect(AddFive(1)).toBe(6)
 });
--- src/componenets/AddFive.js
+++ src/componenets/AddFive.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function AddFive(val) {
-  return console.log (val + Number(5));
+  return (val + Number(5));
 };
 
 export default AddFive;
